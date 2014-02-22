853404dca659c55f4b705bbee526f1dff7424aa7
joshbeam <joshua.a.beam@gmail.com>
2014-02-22 17:11:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Zelektor
+
+A simple JavaScript selector engine (weighs about 830 bytes minified).
+
+Returns an array of one or more DOM elements.
+
+*For limited use in production environments due to lack of browser testing.*
+
+### Use it:
+
+`getElementById`<br>
+`Zelekt('#foo') // => [div#foo]`<br><br>
+
+`getElementsByClassName`<br>
+`Zelekt('.foo') // => [div.foo, div.foo]`<br><br>
+
+`getElementsByName`<br>
+`Zelekt('@foo') // => [div, div]`<br><br>
+
+`getElementsByTagName`<br>
+`Zelekt('div') // => [div, div, div, div]`<br><br>
+
+`querySelectorAll`<br>
+(put an asterisk before whatever CSS2/3-style selector you'd like to use)<br>
+Zelekt('*div > input') // => [input, input]`<br><br>
+
+Also accepts a `DOMElement` for cases in which recursive selection is necessary:<br>
+`Zelekt(document.getElementById('foo')) // => [div#foo]`
+
+### Issues:
+
+- Lack of testing across multiple browsers
+- Only supports CSS-style selectors through `querySelectorAll`
+
+<hr>
+
+The MIT License - (c) 2014, Joshua Beam
---
+++ src/zelektor.js
@@ -0,0 +1,112 @@
+/*
+
+Zelektor
+A simple JavaScript selector engine
+
+The MIT License (c) 2014, Joshua Beam
+
+*/
+
+;(function() {
+	function insertThisBeforeThat(newElement,element) {
+		element.parentElement.insertBefore(newElement,element);
+	}
+
+	function insertThisAfterThat(newElement,element) {
+		element.parentNode.insertBefore(newElement, element.nextSibling);
+	}
+
+	function appendThisToThat(newElement,element) {
+		element.appendChild(newElement);
+	}
+
+	function prependThisToThat(newElement,element) {
+		element.insertBefore(newElement,element.firstChild);
+	}
+
+	function parseStringToHTML(string) {
+		var wrapper = document.createElement('div'),
+			newElement;
+
+		wrapper.innerHTML = string;
+		newElement = wrapper.firstChild;
+
+		return newElement;
+	}
+
+	return function Zelekt() {
+		var args = arguments,
+			len = args.length,
+			selector = args[0],
+			method,
+			context,
+			where,
+			location,
+			newElement,
+			element;
+
+		if(len<3) {
+
+//			Handle Zelekt( selector = string, context = string|undefined );
+			if(typeof selector === 'string') {
+
+				context = args[1] || document;
+
+//				Map the first character of the selector string to a method, like getElementById
+//				Similar to switch, case, default, but uses a plain object instead
+				method = {
+//					'case'
+					'#':'getElementById',
+					'.':'getElementsByClassName',
+					'@':'getElementsByName',
+					'*':'querySelectorAll'
+//									'default'
+				}[selector[0]] || 'getElementsByTagName';
+
+//				If 'getElementsByTagName', selector is the full string (e.g., 'div')
+//				If any other method, selector is the full string minues the first char (e.g. someClass)
+				selector = method.indexOf('Tag') > -1 ? selector : selector.slice(1);
+
+//				For example, document['getElementById']('myDiv')
+				element = context[method](selector);
+
+//				The element is made into an array, regardless of the number of elements
+				element = element.nodeType === 1 ? [element] : [].slice.call(element);
+
+//			Handle Zelekt(DOMElement)
+			} else if (selector.nodeType === 1) {
+
+				element = [selector];
+
+			}
+
+//			Handle Zelekt( selector = HTML string , where = appendTo|prependTo|before|after, location = Zelekt('foo') );
+//			For example, Zelekt('<div id="someID" class="someClass"></div>','appendTo',Zelekt('div'));
+		} else if (len===3) {
+			where = args[1];
+			location = args[2][0];
+			newElement = parseStringToHTML(selector);
+
+			switch(where) {
+				case 'appendTo':
+					appendThisToThat(newElement,location);
+					break;
+				case 'prependTo':
+					prependThisToThat(newElement,location);
+					break;
+				case 'before':
+					insertThisBeforeThat(newElement,location);
+					break;
+				case 'after':
+					insertThisAfterThat(newElement,location);
+					break;
+			}
+
+			element = [newElement];
+
+		}
+
+		return element;
+
+	}
+}());
